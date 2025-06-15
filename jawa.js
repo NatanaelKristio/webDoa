2ec6c8e37a78bb3efe3a7b19f63a62fd5a25f66e
NatanaelKristio <natanaelkristio@gmail.com>
2025-06-15 13:31:21 +0700

    masuk

---
+++ jawa.js
@@ -0,0 +1,7 @@
+function contact() {
+    alert("Thank you for reaching out! You can message us at doamubersamaku@cafe.com");
+  }
+
+  function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
